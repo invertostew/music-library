1be77cadbd7181c659b062055936627e6e5250a3
Stuart Green <github.47a8z@aleeas.com>
2022-02-17 21:08:55 +0000

    created an artist

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,5 +1,19 @@
-const createArtistController = (req, res) => {
-  res.sendStatus(201);
+const getDb = require('../services/db');
+
+const createArtistController = async (req, res) => {
+  const db = await getDb();
+
+  try {
+    await db.query(`INSERT INTO Artist (name, genre)
+      VALUES ('${req.body.name}', '${req.body.genre}')
+    `);
+
+    res.status(201).json({ result: 'Created artist' });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+
+  db.close();
 };
 
 module.exports = {
--- utils/create-database.js
+++ utils/create-database.js
@@ -21,7 +21,6 @@ const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT } = process.env;
 // This asyncronous function will run before app
 const setUpDatabase = async () => {
   try {
-
     // connect to the database
     const db = await mysql.createConnection({
       host: DB_HOST,
@@ -32,10 +31,16 @@ const setUpDatabase = async () => {
 
     // create the database if it doesn't already exist
     await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
+    await db.query(`USE ${DB_NAME}`);
+    // create Artist table
+    await db.query(`CREATE TABLE IF NOT EXISTS Artist (
+      id INT PRIMARY KEY auto_increment,
+      name VARCHAR(25),
+      genre VARCHAR(25)
+    )`);
     db.close();
-
   } catch (err) {
-   // if something goes wrong, console.log the error and the current environment variables
+    // if something goes wrong, console.log the error and the current environment variables
     console.log(
       `Your environment variables might be wrong. Please double check .env file`
     );
