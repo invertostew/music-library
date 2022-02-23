51e34647a90045638882dcde1717dd8e2f5c9201
Stuart Green <github.47a8z@aleeas.com>
2022-02-23 19:29:28 +0000

    heroku setup

--- utils/create-database.js
+++ utils/create-database.js
@@ -10,28 +10,33 @@ const args = process.argv.slice(2)[0];
 // use args to determine if .env or .env.test should be loaded
 const envFile = args === 'test' ? '../.env.test' : '../.env';
 
-// load environment variables from env files
-require('dotenv').config({
-  path: path.join(__dirname, envFile)
-});
+// no dotenv for heroku
+if (args === 'test') {
+  // load environment variables from env files
+  require('dotenv').config({
+    path: path.join(__dirname, envFile)
+  });
+}
 
 // destructure environment variables from process.env
-const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT } = process.env;
+const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT, CLEARDB_DATABASE_URL } = process.env;
 
 // This asyncronous function will run before app
 const setUpDatabase = async () => {
   try {
     // connect to the database
-    const db = await mysql.createConnection({
-      host: DB_HOST,
-      user: DB_USER,
-      password: DB_PASSWORD,
-      port: DB_PORT
-    });
+    const db = CLEARDB_DATABASE_URL
+      ? await mysql.createConnection(CLEARDB_DATABASE_URL)
+      : await mysql.createConnection({
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        port: DB_PORT
+      });
 
     // create the database if it doesn't already exist
-    await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
-    await db.query(`USE ${DB_NAME}`);
+    !CLEARDB_DATABASE_URL && await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
+    !CLEARDB_DATABASE_URL && await db.query(`USE ${DB_NAME}`);
     // create Artist table
     await db.query(`CREATE TABLE IF NOT EXISTS Artist (
       id INT PRIMARY KEY auto_increment,
